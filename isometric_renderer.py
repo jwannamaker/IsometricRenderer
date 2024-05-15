3378d76ae237bddeed82187c7dd5dedb83a208ec
Johnny Wannamaker <j.wannamaker19@gmail.com>
2024-05-15 11:55:55 -0700

    All essential GUI elements are placed and sized appropriately, everything's looking pretty much exactly like I envisioned, and now I'll be moving on to the functionality behind the controls. I need to make sure the rotation matrices are being handled properly.

--- isometric_renderer.py
+++ isometric_renderer.py
@@ -6,10 +6,8 @@ import json
 import itertools
 import tkinter as tk
 from tkinter import ttk
-from tkinter.ttk import Notebook
-from tkinter import colorchooser
 from tkinter import filedialog
-from tkinter import scrolledtext
+from tkinter.ttk import Notebook
 
 import numpy as np
 import matplotlib as mpl
@@ -25,27 +23,31 @@ Assuming start orientation is
             
         -y  
 """
-
-def x_axis_rotation(vertex, angle_degrees):
-    theta = np.deg2rad(angle_degrees)
+def x_axis_rotation(vertices, angle):
+    """ 
+    vertices: 2D np.array([[x_i, y_i, z_i], [x_2, y_2, z_2] ... [x_f, y_f, z_f]])
+    
+    angle: an angle in degrees to rotate the vertices by.
+    """
+    theta = np.deg2rad(angle)
     rot_x = np.array([[1, 0, 0],
                       [0, np.cos(theta), -np.sin(theta)],
                       [0, np.sin(theta), np.cos(theta)]])
-    return np.matmul(rot_x, vertex)
+    return [np.matmul(rot_x, v) for v in vertices]
 
-def y_axis_rotation(vertex, angle_degrees):
-    theta = np.deg2rad(angle_degrees)
+def y_axis_rotation(vertices, angle):
+    theta = np.deg2rad(angle)
     rot_y = np.array([[np.cos(theta), 0, -np.sin(theta)],
                       [0, 1, 0],
                       [np.sin(theta), 0, np.cos(theta)]])
-    return np.matmul(rot_y, vertex)
+    return [np.matmul(rot_y, v) for v in vertices]
 
-def z_axis_rotation(vertex, angle_degrees):
-    theta = np.deg2rad(angle_degrees)
+def z_axis_rotation(vertices, angle):
+    theta = np.deg2rad(angle)
     rot_z = np.array([[np.cos(theta), -np.sin(theta), 0],
                       [np.sin(theta), np.cos(theta), 0],
                       [0, 0, 1]])
-    return np.matmul(rot_z, vertex)
+    return [np.matmul(rot_z, v) for v in vertices]
 
 def isometric(vertex):
     vertex = z_axis_rotation(vertex, 45)
@@ -57,76 +59,85 @@ class IsometricRenderer:
     def __init__(self, root):
         self.root = root
         root.title('Isometric Render Tool')
-        paddings = {'padx': 20, 'pady': 20}
+        paddings = {'padx': 5, 'pady': 5}
+        style = ttk.Style()
+        print(style.theme_names())
+        
         
         pyplot.style.use('dark_background')
         self.fig, self.ax = pyplot.subplots()
-        self.fig.set_figheight(2.56)
-        self.fig.set_figwidth(2.56)
+        self.fig.set_figheight(3)
+        self.fig.set_figwidth(3)
         self.ax.set_axis_off()
         self.ax.set_aspect('equal')
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
         self.widget = self.canvas.get_tk_widget()
-        self.widget.grid(row=2, column=0, columnspan=4)
+        self.widget.grid(row=0, rowspan=4, column=0, columnspan=4)
+        
         
         self.shape_choice = tk.StringVar()
         shape_options = ['Tetrahedron', 'Hexahedron', 'Octahedron', 'Triangle', 'Square']
         self.shape_menu = ttk.OptionMenu(self.root, self.shape_choice, shape_options[0], *shape_options, 
                                          command=self.load_shape)
-        self.shape_menu.grid(row=0, column=0, columnspan=2, sticky='EW', **paddings)
+        self.shape_menu.grid(row=0, column=4, columnspan=2, sticky='NEW', **paddings)
         
-        self.display_choice = tk.StringVar()
-        display_options = ['2D', 'Isometric', '3D']
-        self.display_menu = ttk.OptionMenu(self.root, self.display_choice, display_options[0], *display_options, 
-                                           command=self.render)
-        self.display_menu.grid(row=0, column=2, columnspan=2, sticky='EW', **paddings)
         
         render_button = ttk.Button(self.root, text='Render', command=self.render)
-        render_button.grid(row=0, column=4, sticky='E', **paddings)
+        render_button.grid(row=0, column=6, sticky='NEW', **paddings)
         
         
-        slider_frame = ttk.Frame(self.root)
-        slider_frame.grid(row=2, column=4, sticky='NS', **paddings)
-        
+        self.x_degrees_entry = tk.StringVar()
+        x_label = ttk.Entry(self.root, justify='center', textvariable=self.x_degrees_entry)
+        x_label.grid(row=3, column=4, sticky='SEW', **paddings)
         self.x_degrees = tk.DoubleVar()
-        x_slider = ttk.Scale(slider_frame, variable=self.x_degrees, 
-                             length=450,
-                             from_=-1, to=1, orient='vertical',
-                             command=self.rotate)
-        x_slider.grid(row=0, column=0, sticky='NS', **paddings)
-            
-        self.y_degrees = tk.DoubleVar()
-        y_slider = ttk.Scale(slider_frame, variable=self.y_degrees, 
-                             length=450,
-                             from_=-1, to=1, orient='vertical',
-                             command=self.rotate)
-        y_slider.grid(row=0, column=1, sticky='NS', **paddings)
+        x_slider = ttk.Scale(self.root, variable=self.x_degrees, length=450,
+                             from_=1, to=-1, orient='vertical',
+                             command=self.x_rotate)
+        x_slider.grid(row=1, rowspan=2, column=4, sticky='NS', **paddings)
+        self.x_rotate()
         
+        
+        self.y_degrees_entry = tk.StringVar()
+        y_label = ttk.Entry(self.root, justify='center', textvariable=self.y_degrees_entry)
+        y_label.grid(row=3, column=5, sticky='SEW', **paddings)
+        self.y_degrees = tk.DoubleVar()
+        y_slider = ttk.Scale(self.root, variable=self.y_degrees, length=450,
+                             from_=1, to=-1, orient='vertical',
+                             command=self.y_rotate)
+        y_slider.grid(row=1, rowspan=2, column=5, sticky='NS', **paddings)
+        self.y_rotate()
+        
+        
+        self.z_degrees_entry = tk.StringVar()
+        z_label = ttk.Entry(self.root, justify='center', textvariable=self.z_degrees_entry)
+        z_label.grid(row=3, column=6, sticky='SEW', **paddings)
         self.z_degrees = tk.DoubleVar()
-        z_slider = ttk.Scale(slider_frame, variable=self.z_degrees, 
-                             length=450,
-                             from_=-1, to=1, orient='vertical',
-                             command=self.rotate)
-        z_slider.grid(row=0, column=2, sticky='NS', **paddings)
+        z_slider = ttk.Scale(self.root, variable=self.z_degrees, length=450,
+                             from_=1, to=-1, orient='vertical',
+                             command=self.z_rotate)
+        z_slider.grid(row=1, rowspan=2, column=6, sticky='NS', **paddings)
+        self.z_rotate()
+        
         
         self.shape_file = 'shapes.json'
         self.shape_pts = {}
         self.render_shape = np.array([[]])
         self.load_shapes()
     
-    def rotate(self, *args):
-        # Normalize 
-        
-        # Apply x rotation
-        self.render_shape = x_axis_rotation()
-        
-        # Apply y rotation
-        
-        
-        # Apply z rotation
-        
-        
-        self.render()
+    def x_rotate(self, *args):
+        degrees = self.x_degrees.get() * 360.0
+        self.x_degrees_entry.set(f'{degrees:.2f}')
+        # self.render_shape = x_axis_rotation(self.render_shape, degrees)
+    
+    def y_rotate(self, *args):
+        degrees = self.y_degrees.get() * 360.0
+        self.y_degrees_entry.set(f'{degrees:.2f}')
+        # self.render_shape = y_axis_rotation(self.render_shape, degrees)
+    
+    def z_rotate(self, *args):
+        degrees = self.z_degrees.get() * 360.0
+        self.z_degrees_entry.set(f'{degrees:.2f}')
+        # self.render_shape = z_axis_rotation(self.render_shape, degrees)
     
     def render(self, *args):
         self.ax.clear()
@@ -138,17 +149,13 @@ class IsometricRenderer:
         y = [pt[1] for pt in self.render_shape]
         z = [pt[2] for pt in self.render_shape]
         
-        if self.display_choice.get() == '2D':
-            self.ax.plot(x, y)
-        elif self.display_choice.get() == 'Isometric':
-            self.ax.plot(x, y)
-        elif self.display_choice.get() == '3D':
-            self.ax.plot(x, y, z)
+        self.ax.plot(x, y)
         
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
         self.widget = self.canvas.get_tk_widget()
-        self.widget.grid(row=2, column=0, columnspan=4)
+        self.widget.grid(row=0, rowspan=4, column=0, columnspan=4)
         self.canvas.draw()
+        print(self.canvas.get_width_height())
     
     def get_filename(self, full_path):
         i = full_path.rfind('/') + 1
