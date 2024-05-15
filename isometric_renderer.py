d2bebf1c7f9635f1ce01e864a4aa77db16e4273d
Johnny Wannamaker <j.wannamaker19@gmail.com>
2024-05-14 18:20:21 -0700

    There's now 3 sliders corresponding to the x, y, and z axis rotation amount along their respective axes. I still need to implement EntryBoxes at the bottom of each one, that will also set the position of the sldier according to the text in the entry box. Also actually applying each rotation, I imagine, would be pretty helpful.

--- isometric_renderer.py
+++ isometric_renderer.py
@@ -67,7 +67,7 @@ class IsometricRenderer:
         self.ax.set_aspect('equal')
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
         self.widget = self.canvas.get_tk_widget()
-        self.widget.grid(row=2, column=0, columnspan=5)
+        self.widget.grid(row=2, column=0, columnspan=4)
         
         self.shape_choice = tk.StringVar()
         shape_options = ['Tetrahedron', 'Hexahedron', 'Octahedron', 'Triangle', 'Square']
@@ -75,59 +75,105 @@ class IsometricRenderer:
                                          command=self.load_shape)
         self.shape_menu.grid(row=0, column=0, columnspan=2, sticky='EW', **paddings)
         
-        
-        self.shape_file = 'shapes.json'
-        self.shape_pts = {}
-        self.load_shape(*shape_options)
-        
-        
         self.display_choice = tk.StringVar()
-        display_options = ['2D', 'Isometric', '3D (in progress)']
+        display_options = ['2D', 'Isometric', '3D']
         self.display_menu = ttk.OptionMenu(self.root, self.display_choice, display_options[0], *display_options, 
                                            command=self.render)
         self.display_menu.grid(row=0, column=2, columnspan=2, sticky='EW', **paddings)
         
         render_button = ttk.Button(self.root, text='Render', command=self.render)
-        render_button.grid(row=0, column=4, sticky='EW', **paddings)
+        render_button.grid(row=0, column=4, sticky='E', **paddings)
+        
+        
+        slider_frame = ttk.Frame(self.root)
+        slider_frame.grid(row=2, column=4, sticky='NS', **paddings)
+        
+        self.x_degrees = tk.DoubleVar()
+        x_slider = ttk.Scale(slider_frame, variable=self.x_degrees, 
+                             length=450,
+                             from_=-1, to=1, orient='vertical',
+                             command=self.rotate)
+        x_slider.grid(row=0, column=0, sticky='NS', **paddings)
+            
+        self.y_degrees = tk.DoubleVar()
+        y_slider = ttk.Scale(slider_frame, variable=self.y_degrees, 
+                             length=450,
+                             from_=-1, to=1, orient='vertical',
+                             command=self.rotate)
+        y_slider.grid(row=0, column=1, sticky='NS', **paddings)
+        
+        self.z_degrees = tk.DoubleVar()
+        z_slider = ttk.Scale(slider_frame, variable=self.z_degrees, 
+                             length=450,
+                             from_=-1, to=1, orient='vertical',
+                             command=self.rotate)
+        z_slider.grid(row=0, column=2, sticky='NS', **paddings)
+        
+        self.shape_file = 'shapes.json'
+        self.shape_pts = {}
+        self.render_shape = np.array([[]])
+        self.load_shapes()
+    
+    def rotate(self, *args):
+        # Normalize 
+        
+        # Apply x rotation
+        self.render_shape = x_axis_rotation()
+        
+        # Apply y rotation
+        
+        
+        # Apply z rotation
+        
+        
+        self.render()
     
     def render(self, *args):
         self.ax.clear()
         self.ax.set_axis_off()
         self.ax.set_aspect('equal')
         
-        render_shape = self.shape_pts[self.shape_choice.get()]
-        x = [pt[0] for pt in render_shape]
-        y = [pt[1] for pt in render_shape]
-        z = [pt[2] for pt in render_shape]
+        self.render_shape = self.shape_pts[self.shape_choice.get()]
+        x = [pt[0] for pt in self.render_shape]
+        y = [pt[1] for pt in self.render_shape]
+        z = [pt[2] for pt in self.render_shape]
         
-        self.ax.plot(x, y)
-        
+        if self.display_choice.get() == '2D':
+            self.ax.plot(x, y)
+        elif self.display_choice.get() == 'Isometric':
+            self.ax.plot(x, y)
+        elif self.display_choice.get() == '3D':
+            self.ax.plot(x, y, z)
         
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
         self.widget = self.canvas.get_tk_widget()
-        self.widget.grid(row=2, column=0, columnspan=5)
+        self.widget.grid(row=2, column=0, columnspan=4)
         self.canvas.draw()
-    
-    def permute(self):
-        
-        self.shape_pts 
-        
-        self.render()
-        
     
     def get_filename(self, full_path):
         i = full_path.rfind('/') + 1
         return full_path[i:]
     
-    def load_shape(self, *args):
-        with open(self.shape_file) as f:
+    def load_shapes(self):
+        with open(self.shape_file, 'r') as f:
             self.shape_pts = json.load(f)
     
-    def save_shape(self):
-        self.shape_file = filedialog.asksaveasfile(parent=self.root, 
-                                                   initialfile=self.get_filename(self.shape_file))
-        with open(self.shape_file, 'w') as f:
+    def load_shape(self, *args):
+        with open(self.shape_file, 'r') as f:
+            file_dict = json.load(f)
+            if len(args) and args[0] in list(file_dict.keys()):
+                self.shape_pts[args[0]] = file_dict[args[0]]
+    
+    def save_shapes(self):
+        save_file = filedialog.asksaveasfile(parent=self.root)
+        with open(save_file, 'w') as f:
             json.dump({self.shape_choice.get(): self.shape_pts}, f, indent=4)
+    
+    def save_shape(self, *args):
+        save_file = filedialog.asksaveasfile(parent=self.root)
+        with open(save_file, 'w') as f:
+            json.dump({self.shape_choice.get(): self.shape_pts}, f, indent=4)
+    
     
 if __name__ == '__main__':
     root = tk.Tk()
--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-    iso_points = [isometric(v) for v in self.shape]
-            self.ax.plot(iso_points[0], iso_points[1])
